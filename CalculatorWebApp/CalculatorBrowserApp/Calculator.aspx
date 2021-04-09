f094c80ccbc25dbf719984c86b0c86eef8301d03
Jubair-Ahmed-Junjun <jubair.ahmed.junjun@gmail.com>
2021-04-09 20:06:48 +0600

    first web app(create a calculator)

---
+++ CalculatorWebApp/CalculatorBrowserApp/Calculator.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="CalculatorBrowserApp.Calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="label" runat="server" Text="First Number :"></asp:Label>
+            <asp:TextBox ID="firstNumTextBox" runat="server" Height="16px" Width="124px"></asp:TextBox>
+            <br /> <br />
+            <asp:Label ID="label1" runat="server" Text="Second Number :"></asp:Label>
+            <asp:TextBox ID="secondNumTextBox" runat="server" Height="16px" Width="124px"></asp:TextBox>
+             <br /> <br />
+            <asp:Label ID="label2" runat="server" Text="Result :"></asp:Label>
+            <asp:TextBox ID="resultTextBox" runat="server" Height="16px" Width="124px"></asp:TextBox>
+             <br /> <br />
+            <asp:Button ID="addButton" runat="server" Text="+" OnClick="addButton_Click" />
+            <asp:Button ID="subButton" runat="server" Text="-" OnClick="subButton_Click" />
+            <asp:Button ID="mulButton" runat="server" Text="*" OnClick="mulButton_Click" />
+            <asp:Button ID="divButton" runat="server" Text="/" OnClick="divButton_Click" />
+            <asp:Button ID="reminderButton" runat="server" Text="%" OnClick="reminderButton_Click" />
+            <asp:Button ID="clearButton" runat="server" Text="C" OnClick="clearButton_Click" />
+        </div>
+       
+      
+    </form>
+</body>
+</html>
---
+++ CalculatorWebApp/CalculatorBrowserApp/Calculator.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalculatorBrowserApp
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            int firstNum = Convert.ToInt32(firstNumTextBox.Text.ToString());
+            int secondNum = Convert.ToInt32(secondNumTextBox.Text.ToString());
+            int add = firstNum + secondNum;
+            resultTextBox.Text = add.ToString();
+        }
+
+        protected void subButton_Click(object sender, EventArgs e)
+        {
+            int firstNum = Convert.ToInt32(firstNumTextBox.Text.ToString());
+            int secondNum = Convert.ToInt32(secondNumTextBox.Text.ToString());
+            int sub = firstNum - secondNum;
+            resultTextBox.Text = sub.ToString();
+        }
+
+        protected void mulButton_Click(object sender, EventArgs e)
+        {
+            int firstNum = Convert.ToInt32(firstNumTextBox.Text.ToString());
+            int secondNum = Convert.ToInt32(secondNumTextBox.Text.ToString());
+            int mul = firstNum * secondNum;
+            resultTextBox.Text = mul.ToString();
+        }
+
+        protected void divButton_Click(object sender, EventArgs e)
+        {
+            double firstNum = Convert.ToDouble(firstNumTextBox.Text.ToString());
+            double secondNum = Convert.ToDouble(secondNumTextBox.Text.ToString());
+            double div = firstNum / secondNum;
+            resultTextBox.Text = div.ToString();
+        }
+
+        protected void reminderButton_Click(object sender, EventArgs e)
+        {
+            int firstNum = Convert.ToInt32(firstNumTextBox.Text.ToString());
+            int secondNum = Convert.ToInt32(secondNumTextBox.Text.ToString());
+            int rem = firstNum % secondNum;
+            resultTextBox.Text = rem.ToString();
+        }
+
+        protected void clearButton_Click(object sender, EventArgs e)
+        {
+            resultTextBox.Text = string.Empty;
+            firstNumTextBox.Text = string.Empty;
+            secondNumTextBox.Text = string.Empty;
+        }
+    }
+}
